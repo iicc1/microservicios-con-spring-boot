fcfdef302af91bad9c736cbcf5d164b665a7a700
Eva Sanchez <evasga98@gmail.com>
2020-12-28 16:51:32 +0100

    Añadido microservicio Cursos

---
+++ microserviciocursos/src/main/java/com/cpcd/microservices/app/cursos/services/ServicioCurso.java
@@ -0,0 +1,16 @@
+package com.cpcd.microservices.app.cursos.services;
+
+import java.util.Optional;
+
+import com.cpcd.microservices.app.cursos.models.entity.Curso;
+
+public interface ServicioCurso {
+	
+	public Iterable<Curso> findAll();
+	
+	public Optional<Curso> findById(Long id);
+	
+	public Curso save(Curso curso);
+	
+	public void deleteById(Long id);
+}
