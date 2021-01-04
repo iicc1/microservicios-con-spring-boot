639f06f2ec39fa5e5c7e0896b540c304c05b8202
kovaxs <oleksandr.kovalyk8@gmail.com>
2021-01-04 10:35:25 +0100

    Primer commit microservicio estudiante

---
+++ microservicoestudiante/src/main/java/com/cpcd/microservices/app/estudiante/MicroservicoestudianteApplication.java
@@ -0,0 +1,13 @@
+package com.cpcd.microservices.app.estudiante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicoestudianteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicoestudianteApplication.class, args);
+	}
+
+}
---
+++ microservicoestudiante/src/test/java/com/cpcd/microservices/app/estudiante/MicroservicoestudianteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpcd.microservices.app.estudiante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicoestudianteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
