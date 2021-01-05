d8955c8209420e17eb32fae9a218ce8d98640504
Ignacio Iglesias Castreño <ignacio.iglesias@hotmail.es>
2021-01-06 00:20:31 +0100

    create microservice project "unidades"

---
+++ microserviciounidades/src/main/java/com/cpcd/microservices/app/unidades/MicroserviciounidadesApplication.java
@@ -0,0 +1,13 @@
+package com.cpcd.microservices.app.unidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciounidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciounidadesApplication.class, args);
+	}
+
+}
