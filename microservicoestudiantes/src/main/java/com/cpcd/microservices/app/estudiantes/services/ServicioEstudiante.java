308c8c6b87c7afdb0713c1b15b043bc0a58bd398
kovaxs <oleksandr.kovalyk8@gmail.com>
2021-01-06 14:30:53 +0100

    Terminada la parte de Servicio

---
+++ microservicoestudiantes/src/main/java/com/cpcd/microservices/app/estudiantes/services/ServicioEstudiante.java
@@ -0,0 +1,16 @@
+package com.cpcd.microservices.app.estudiantes.services;
+
+import java.util.Optional;
+
+import com.cpcd.microservices.app.estudiantes.models.entity.Estudiante;
+
+public interface ServicioEstudiante {
+
+	public Iterable<Estudiante> findAll();
+	
+	public Optional<Estudiante> findById(Long id);
+	
+	public Estudiante save(Estudiante estudiante);
+	
+	public void deleteById(Long id);
+}
